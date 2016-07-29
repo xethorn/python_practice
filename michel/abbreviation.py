6ccf7e7f1dba79766674f81433d955f7f2752fa6
Michael Ortali <github@xethorn.net>
2016-07-29 18:07:07 -0400

    Update abbreviation.py

--- michel/abbreviation.py
+++ michel/abbreviation.py
@@ -3,19 +3,10 @@
 
 class Abbrev():
     def __init__(self, string):
-        self.string = string
+        self.string = string.upper()
 
+    @property
     def initials(self):
-        initials = []
-        count = 0
-        self.string = self.string.upper()
-        # Putting the first letter of the string into the list
-        initials.append(self.string[0])
-
-        for letter in self.string:
-            # Checking every letter for a space
-            # and assigning the next value to a list
-            if self.string[count] == ' ':
-                initials.append(' '+self.string[count+1])
-            count += 1
-        return ''.join(initials)
+        return ''.join([
+            word.strip()[0] for word in self.string.split(' ')
+            if word.strip()]
