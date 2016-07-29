7d9c84736826d7e1008bfcaaa0f6d441f3ef254c
stvngrcia <stevengarciaacuna@gmail.com>
2016-07-29 07:59:26 -0700

    added a typeError if split fails

--- michel/test.py
+++ michel/test.py
@@ -13,11 +13,15 @@ class abbrev_test(unittest.TestCase):
         # checking if it is a string
         obj = Abbrev('steven garcia acuna')
         self.assertEqual(obj.initials().split(), ['S', 'G', 'A'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            obj.initials().split(2)
 
     def test_three(self):
         # Checking for the right initials
         obj = Abbrev('checking initials')
         assert obj.initials() == 'C I'
+
 
 if __name__ == '__main__':
     unittest.main()
